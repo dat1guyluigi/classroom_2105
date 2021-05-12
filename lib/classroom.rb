5be98762e8f744a3dce46ff82ffa237db494fc80
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-05-12 15:51:26 -0600

    add assignment

--- lib/classroom.rb
+++ lib/classroom.rb
@@ -1,2 +1,14 @@
 class Classroom
+  attr_reader :subject, :capacity, :students
+
+  def initialize(subject, capacity, students)
+    @subject = subject
+    @capacity = capacity
+    @students = students
+
+  end
+def add_student(student)
+  @students << students
+end
+
 end
